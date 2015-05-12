10c494cdb12d00456939421ade085529987de80e
Rafal <kenorb@gmail.com>
2015-05-12 22:06:24 +0100

    Improving logging and added functions to generate random strings.

--- selenium/AdsTest.py
+++ selenium/AdsTest.py
@@ -15,6 +15,7 @@ from xvfbwrapper import Xvfb
 
 import unittest
 import logging
+import string
 import time
 import re
 from base64 import b64encode
@@ -36,13 +37,14 @@ class AdsTest(unittest.TestCase):
 
         # Set up the browser driver.
         browsers = {
-            'Firefox':  webdriver.Firefox,
-            'Chrome':   webdriver.Chrome,
-            'IE':       webdriver.Ie,
-            'Opera':    webdriver.Opera,
+            'Firefox':      webdriver.Firefox,
+            'Chrome':       webdriver.Chrome,
+            'IE':           webdriver.Ie,
+            'Opera':        webdriver.Opera,
+            'PhantomJS':    webdriver.PhantomJS,
         }
         # Assign the driver in order: Environment variable, argument value, otherwise Firefox as default.
-        self.site = browsers.get(os.environ.get('SELENIUM_BROWSER', self.args.browser), webdriver.Firefox)()
+        self.driver = browsers.get(os.environ.get('SELENIUM_BROWSER', self.args.browser), webdriver.Firefox)()
 
         # Set-up testing URL.
         self.base_url = os.environ.get('SELENIUM_HOST', self.args.host)
@@ -52,24 +54,36 @@ class AdsTest(unittest.TestCase):
             #self.headers = { 'Authorization': 'Basic %s' % b64encode(bytes(self.args.user + ':' + self.args.password, "utf-8")).decode("ascii") }
 
         # Set-up other settings.
-        self.site.implicitly_wait(30)
+        self.driver.implicitly_wait(30)
         self.verificationErrors = []
         self.accept_next_alert = True
-        self.site.maximize_window()
+        self.driver.maximize_window()
+
+    def is_text_present(self, text):
+        self.log.info("Checking if text '{0}' is present on the page.".format(text))
+        return str(text) in self.driver.page_source
 
     def is_element_present(self, how, what):
-        try: self.site.find_element(by=how, value=what)
+        self.log.info("Checking if '{0}' element is present in '{1}' on the page.".format(what, how))
+        try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
 
     def is_alert_present(self):
-        try: self.site.switch_to_alert()
+        self.log.info("Checking if alert is present.")
+        try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
 
+    def random_word(length=6, chars=string.ascii_lowercase):
+        return ''.join(random.choice(chars) for i in range(length))
+
+    def random_number(length=3):
+        return ''.join(random.choice(string.digits) for i in range(length))
+
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.site.switch_to_alert()
+            alert = self.driver.switch_to_alert()
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
@@ -79,6 +93,7 @@ class AdsTest(unittest.TestCase):
         finally: self.accept_next_alert = True
 
     def wait_for(self, condition_function):
+        self.log.info("Waiting for {0}".format(condition_function))
         start_time = time.time()
         while time.time() < start_time + 5:
             if condition_function():
@@ -88,11 +103,12 @@ class AdsTest(unittest.TestCase):
         raise Exception('Timeout waiting for {}'.format(condition_function.__name__))
 
         def tearDown(self):
-            self.site.quit()
+            self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
     def page_has_loaded(self):
-        page_state = self.site.execute_script('return document.readyState;')
+        self.log.info("Checking if page is loaded.")
+        page_state = self.driver.execute_script('return document.readyState;')
         return page_state == 'complete'
 
     def is_verbose(self):
@@ -111,9 +127,15 @@ class AdsTest(unittest.TestCase):
         print("INFO: ", msg) if self.is_verbose else ''
 
     def make_screenshot(self, filename='screenshot'):
-        self.site.save_screenshot(filename)
-        log.debug('Screenshot saved.')
+        self.driver.save_screenshot(filename)
+        self.log.debug("Screenshot saved as {0}.".format(filename))
+
+    def tearDown(self):
+        self.log.debug('Finishing up.')
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
 
     def kill(self):
+        self.log.debug('Closing display.')
         self.vdisplay.stop()
 
