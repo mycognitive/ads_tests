d443bb1444e0e863c579c3bdbeecfb2309a872a2
Rafal <kenorb@gmail.com>
2015-05-16 23:11:04 +0100

    Renaming registration.py and simplifying imports.

--- selenium/tests/users/auth/registration.py
+++ selenium/tests/users/auth/registration.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
 from BaseTest import BaseTest
-
 import time
 
 class AdsTestRegistration(BaseTest):
