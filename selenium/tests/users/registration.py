4ba8b2d6dc73fecd8e13e30d72d00f53d22cd597
Rafal <kenorb@gmail.com>
2015-05-16 21:33:06 +0100

    Populating additional fields during registration after settings changes.

--- selenium/tests/users/registration.py
+++ selenium/tests/users/registration.py
@@ -16,13 +16,13 @@ class AdsTestRegistration(AdsTest):
         self.is_error_not_present()
         ymd = time.strftime("%Y%m%d")
         username = "test_" + self.random_word() + "_" + ymd
+        password = "pass4" + username
         email = self.random_word() + "_" + ymd +  "@example.com"
-        driver.find_element_by_id("edit-name--2").clear()
         driver.find_element_by_id("edit-name--2").send_keys(username)
-        driver.find_element_by_id("edit-mail").clear()
         driver.find_element_by_id("edit-mail").send_keys(email)
-        driver.find_element_by_id("edit-conf-mail").clear()
         driver.find_element_by_id("edit-conf-mail").send_keys(email)
+        driver.find_element_by_id("edit-pass-pass1").send_keys(password)
+        driver.find_element_by_id("edit-pass-pass2").send_keys(password)
         driver.find_element_by_id("edit-submit--2").click()
         self.assertTrue(self.is_text_present("Thank you for applying for an account."))
         self.assertTrue(driver.find_element_by_xpath(u'//a[text()="Site map"]'))
